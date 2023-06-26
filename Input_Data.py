c9fc4e1a382c09454fca23b166624a8a3d15ea78
AmaruIzarra <113385998+AmaruIzarra@users.noreply.github.com>
2023-06-26 16:27:32 -0400

    Add files via upload

---
+++ Input_Data.py
@@ -0,0 +1,81 @@
+import csv
+import numpy as np
+
+
+class InputData():
+    """This class takes a file and extracts the data point for the system
+
+    Default wind direction is 270
+    """
+
+    def __init__(self, filename):
+
+        self.file = open(filename)
+        self.reader = csv.reader(self.file)
+
+        self.initial_positions = np.array(get_data_float(self.reader)).T
+
+        temp_data = get_data_float(self.reader)
+        self.ti, self.hub_height = temp_data[0][0], temp_data[0][1]
+
+        temp_data = get_data(self.reader)
+
+        if temp_data[0][0] == 'True':
+            self.w_check = True
+            self.ws = None
+            temp_data = np.array(get_data_3_float(self.reader)).T
+            self.p_wd, self.a, self.k = temp_data[0], temp_data[1], temp_data[2]
+
+        else:
+            self.w_check = False
+            self.ws = float(temp_data[0][1])
+            self.wd = 270
+            temp_data = get_data_3(self.reader)
+            self.p_wd, self.a, self.k = temp_data[0][0], temp_data[0][1], \
+                                        temp_data[0][2]
+
+        self.file.close()
+
+
+def get_data(reader):
+    data = []
+    for line in reader:
+        if not line:
+            break
+        elif not line[0].startswith('#'):
+            data.append([line[0], line[1]])
+
+    return data
+
+
+def get_data_float(reader):
+    data = []
+    for line in reader:
+        if not line:
+            break
+        elif not line[0].startswith('#'):
+            data.append([float(line[0]), float(line[1])])
+
+    return data
+
+
+def get_data_3(reader):
+    data = []
+    for line in reader:
+        if not line:
+            break
+        elif not line[0].startswith('#'):
+            data.append([line[0], line[1], line[2]])
+
+    return data
+
+
+def get_data_3_float(reader):
+    data = []
+    for line in reader:
+        if not line:
+            break
+        if not line[0].startswith('#'):
+            data.append([float(line[0]), float(line[1]), float(line[2])])
+
+    return data
---
+++ main.py
@@ -0,0 +1,65 @@
+import py_wake
+import Input_Data
+from py_wake.examples.data.hornsrev1 import V80
+import matplotlib.pyplot as plt
+from py_wake.site import XRSite
+import numpy as np
+from py_wake import BastankhahGaussian
+from py_wake import XYGrid, YZGrid, XZGrid
+
+
+data = Input_Data.InputData('input_data.csv')
+windTurbines = V80()
+
+if data.w_check is False:
+    my_site = py_wake.site.UniformSite([1], ti=data.ti, ws=data.ws)
+else:
+    my_site = py_wake.site.UniformWeibullSite(p_wd=data.p_wd, a=data.a,
+                                              k=data.k, ti=data.ti)
+
+wf_model = BastankhahGaussian(my_site, windTurbines)
+wt_x, wt_y = data.initial_positions
+
+sim_results = wf_model(wt_x, wt_y, h=data.hub_height, ws=data.ws)
+
+grid = XYGrid(x=np.linspace(0, 700), y=np.linspace(0,700))
+flow_map = sim_results.flow_map(grid=grid, wd=data.wd, ws=data.ws)
+flow_map.plot_wake_map()
+plt.xlabel('x [m]')
+plt.ylabel('y [m]')
+plt.title('XY Wake map for' + f' {data.ws} m/s and {data.wd} deg')
+plt.show()
+
+# x is the value of x that is sliced
+grid = YZGrid(x=600, y=None, resolution=100)
+flow_map = sim_results.flow_map(grid=grid, wd=270, ws=data.ws)
+flow_map.plot_wake_map()
+plt.xlabel('y [m]')
+plt.ylabel('z [m]')
+plt.title('YZ Wake map for' + f' {data.ws} m/s and {270} deg')
+plt.show()
+
+# y is y value to sliced
+grid = XZGrid(y=600, resolution=100)
+flow_map = sim_results.flow_map(grid=grid, wd=data.wd, ws=data.ws)
+flow_map.plot_wake_map()
+plt.xlabel('x [m]')
+plt.ylabel('z [m]')
+plt.title('XZ Wake map for' + f' {data.ws} m/s and {270} deg')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
